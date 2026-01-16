172ffc35f9ff378fa5ffcc31e111f86b07cb8559
Abhay-Pratap200001 <abhaypratap94310@gmail.com>
2026-01-16 12:22:15 +0530

    Next.js configuration and files

---
+++ src/app/components/component.jsx
@@ -0,0 +1,9 @@
+const component = () => {
+  return (
+    <div className="min-h-100 w-full rounded-2xl bg-neutral-100">
+
+    </div>
+  );
+};
+
+export default component;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Component from "@/app/components/component";
+
+const page = () => {
+  return (
+    <main className="mx-auto flex h-screen max-w-2xl items-center">
+      <Component />
+    </main>
+  );
+};
+
+export default page;
